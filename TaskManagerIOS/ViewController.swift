484e0626d08df0e149a644a77953a405f686c09f
Andrew Beauchamp <andrewbeauchamp@macbook-pro-55.local>
2018-11-26 12:01:50 -0600

    Initial Commit

---
+++ TaskManagerIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagerIOS
+//
+//  Created by Andrew Beauchamp on 11/26/18.
+//  Copyright © 2018 Andrew Beauchamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
